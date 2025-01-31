50f6ac63b92fd5ddf61f85457993b713c6c2a56f
anishs13 <anish22td0656@svcet.ac.in>
2025-01-31 13:05:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,5 +6,27 @@ def home():
 @app.route('/hello/<name>')
 def hello(name):
     return f"Hello, {name}!"
+
+
+@app.route('/data', methods=['GET', 'POST'])
+def handle_data():
+    if request.method == 'POST':
+        data = request.json
+        return {"message": "Data received", "data": data}
+    return {"message": "Send a POST request with JSON data"}
+
+
+
+@app.route('/update/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    return {"message": f"Item {item_id} updated"}
+
+@app.route('/delete/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    return {"message": f"Item {item_id} deleted"}
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
