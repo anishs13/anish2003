809e4a84b40f10403cf01269205585ea2bbb39f3
anishs13 <anish22td0656@svcet.ac.in>
2025-01-31 16:00:23 +0530

    Add files via upload

---
+++ Api.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class CSK(Resource):
+    list = []
+
+    def get(self):
+        return {"players": CSK.list}
+
+    def post(self):
+      
+        data = request.get_json() 
+        player = data.get('player') 
+        CSK.list.append(player)
+        return {"message": f"Player {player} added successfully!"}, 
+
+api.add_resource(CSK, '/csk')
+
+if __name__ == '__main__':
+    app.run(debug=True)
